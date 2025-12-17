b0b42d867179c484367dda3a9e8db06c3f20d83b
OkazakiKeita <ca01254004@st.kawahara.ac.jp>
2025-12-17 13:51:59 +0900

    ステージの土台とオブジェクトの点滅の追加 (#9)

---
+++ Assets/Script/BlueCircle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCircle : MonoBehaviour
+{
+    public LightState state = LightState.Blinking;
+    MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        StartCoroutine(StateControl());
+    }
+
+    IEnumerator StateControl()
+    {
+        while (true)
+        {
+            state = LightState.On;
+            mesh.enabled = true;
+            yield return new WaitForSeconds(5f);
+
+            state = LightState.Blinking;
+            float t = 0f;
+            while(t<3f)
+            {
+                mesh.enabled = !mesh.enabled;
+                yield return new WaitForSeconds(0.2f);
+                t += 0.2f;
+            }
+
+            state = LightState.Off;
+            mesh.enabled = false;
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    public bool IsDanger()
+    {
+        return state == LightState.Off;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
