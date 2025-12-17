ad5cb79fc953f62b9f65bdd06640707487b2adaa
MitsumuneJuria <ca01254020@st.kawahara.ac.jp>
2025-12-17 14:28:17 +0900

    ドアの追加

---
+++ Assets/Script/KeyScriptScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScriptScript : MonoBehaviour
+{
+    Vector3 Position;
+    // プレイヤーが触れたときに呼ばれる関数（トリガー判定）
+    void OnTriggerEnter(Collider other)
+    {
+        // 触れたオブジェクトに "Player" タグが付いているかチェック
+        if (other.CompareTag("Untagged"))
+        {
+            Debug.Log("アイテムに触れた！"); // ログを表示
+                                    // TODO: アイテム取得処理やエフェクト再生処理などをここに書く
+
+
+            this.transform.position = new Vector3(8.4f, -1.98f, -0.04f);
+            if (other.CompareTag("Untagged"))
+            {
+                Debug.Log("ロック解除！"); // ログを表示
+                                     // TODO: アイテム取得処理やエフェクト再生処理などをここに書く
+
+
+                //アイテムを消す
+               // Destroy(this.gameObject);
+            }
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
