902bed99b82518f4d0a91d844ea382c44f412391
MinoYasuhiro <CA01254021@st.kawahara.ac.jp>
2025-12-18 10:58:01 +0900

    タイマーの追加 (#17)

---
+++ Assets/Script/TimeCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    private float countup = 0.0f;
+
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countup += Time.deltaTime;
+
+        timeText.text = countup.ToString("F2");
+    }
+}
