f2d89a10f38156c6992b904ac7b6b770a6bcdb5d
MinoYasuhiro <CA01254021@kawahara.ac.jp>
2025-12-19 11:36:23 +0900

    音の追加

---
+++ Assets/Script/BomSeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomSEScript : MonoBehaviour
+{
+
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Bomb"))
+            {
+                //音(sound1)を鳴らす
+                audioSource.PlayOneShot(sound1);
+            }
+        }
+    
+}
