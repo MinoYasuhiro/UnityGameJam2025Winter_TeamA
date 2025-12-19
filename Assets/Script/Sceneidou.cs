19d771f4e7d2bafffd22ebf8aec757fb7ae777e9
MinoYasuhiro <CA01254021@kawahara.ac.jp>
2025-12-19 10:55:55 +0900

    死亡シーン遷移。

---
+++ Assets/Script/Sceneidou.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneidou : MonoBehaviour
+{
+
+    [SerializeField] private string _loadScene;
+    public int _delay; //遅延させたい秒数
+
+
+    void Start()
+    {
+        TimeLag();
+    }
+    public void TimeLag()
+    {
+        Invoke("SceneChange", _delay);
+    }
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(_loadScene);
+    }
+}
